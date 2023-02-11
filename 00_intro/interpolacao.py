e91fbe47fbaf7e921c2af151152523c666d313a3
davigdc <davigdc10@gmail.com>
2023-02-10 23:40:24 -0300

    Uso de interpolação de strings

---
+++ 00_intro/interpolacao.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Imprime a mensagem de um e-mail
+
+NAO MANDE SPAM!!!
+"""
+__version__ = "0.1.0"
+
+email_template = """
+Olá, %(nome)s
+
+Tem interesse em comprar %(produto)s?
+
+Este produto é ótimo para resolver %(texto)s
+
+Clique agora em %(link)s
+
+Apenas %(quantidade)d disponiveis!
+
+Preço promocional %(preco).2f
+"""
+
+clientes = ["Maria", "Joao", "Bruno"]
+
+for cliente in clientes:
+    print(
+        email_template
+        % {
+            "nome": cliente,
+            "produto": "caneta",
+            "texto": "Escrever muito bem",
+            "link": "http://canetaslegais.com",
+            "quantidade": 1,
+            "preco": 50.5,
+        }
+    )
